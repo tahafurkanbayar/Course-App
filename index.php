46a7a0d5e6b525a7b22b7210bedaa270e5eaacb6
Taha Furkan Bayar <tahafurkanbayar@gmail.com>
2024-12-23 00:28:36 +0300

    Giriş testleri yapıldı

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css"/>
-    <title>Document</title>
+    <title>Course-App</title>
 </head>
 <body>
     <div class="container my-3">
